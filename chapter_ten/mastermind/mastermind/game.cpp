5ef88f2410c90597abb0d52c17b760cc868b4e99
Adam Frazee <frazeeat@miamioh.edu>
2015-07-30 09:41:32 -0400

    compare_num fixed
    Hopefully now only gives correct results. More testing required.

--- chapter_ten/mastermind/mastermind/game.cpp
+++ chapter_ten/mastermind/mastermind/game.cpp
@@ -7,13 +7,13 @@ std::vector<int> combo_vector(int a, int b, int c, int d){
 	//Creates a vector from four ints written in.
 	//We are going to assume that the numbers will always be
 	// between 0 and 9.
-	std::vector<int> combo{ d,c,b,a};
+	std::vector<int> combo{ d, c, b, a };
 	return combo;
 }
 
 int game_input(){
 	int a = 0;
-	while (!(std::cin >> a)|| a<0 || a>9 ){
+	while (!(std::cin >> a) || a<0 || a>9){
 		std::cout << "Invaild entry, enter a number between 0 and 9." << std::endl;
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -23,7 +23,7 @@ int game_input(){
 
 int compare_pos_and_num(std::vector<int> a, std::vector<int> b){
 	int pos_num = 0;
-	for (int i=0; i < a.size();i++){
+	for (int i = 0; i < a.size(); i++){
 		if (a[i] == b[i]){
 			pos_num++;
 		}
@@ -32,23 +32,24 @@ int compare_pos_and_num(std::vector<int> a, std::vector<int> b){
 }
 
 int compare_num(std::vector<int> a, std::vector<int> b){
-	int  bulls = compare_pos_and_num(a, b), pos_num=0;
-	if (bulls == 4){
-		return 0;
-	}
+	int pos_num = 0, temp = -1;
+
 	for (int i = 0; i < a.size(); i++){
-		if (a[i] != b[i]){
+		if (a[i] != b[i] && a[i] != temp){
 			if (std::find(b.begin(), b.end(), a[i]) == b.end()){
 			}
 			else{
 				pos_num++;
+				temp = a[i];
 			}
 		}
+		else{
+			pos_num--;
+		}
 	}
-	if (pos_num - bulls < 0){
-		return pos_num;
+	if (pos_num < 0){
+		return 0;
 	}
-	else{
-		return pos_num;
-	}
+	return pos_num;
+
 }
